545919693cf1f26c0bec85a424ef18de327ee959
ejtack1 <ejtack1@gmail.com>
2023-04-27 22:44:12 -0400

    new file:   playgroundCodingSpace/pcs1.go

---
+++ playgroundCodingSpace/pcs1.go
@@ -0,0 +1,9 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Welcome")
+	fmt.Println("I am currently learning go and I hope to improve over the next few months [weeks(?)]")
+	fmt.Println("idk")
+}
